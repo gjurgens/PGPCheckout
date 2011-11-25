b846d99e7e1e4af7a7c83cc83893b301b15317cb
Gabriel Jürgens <gabriel@jurgens.com.ar>
2011-11-25 17:56:56 -0300

    Changes into manager

--- pgpcheckout_manage.php
+++ pgpcheckout_manage.php
@@ -1,1 +1,50 @@
-Administrador
+<?php
+	global $wpdb;	
+	$table_name = $wpdb->prefix . "pgpcheckout_transactions";
+	
+	$transactions = $wpdb->get_results( 
+		"
+		SELECT 
+			id,
+			id_transaction,
+			id_product,
+			status,
+			time,
+			private_data,
+			public_data		
+		FROM $table_name
+		"
+	);
+	
+
+?>
+
+<table>
+	<tr>
+		<th>Id</th>
+		<th>Transaction</th>
+		<th>Product</th>
+		<th>Status</th>
+		<th>Date</th>
+		<th>Private</th>
+		<th>Public</th>
+	</tr>
+	<?php 
+	foreach ( $transactions as $transaction ) 
+	{
+	?>
+	<tr>
+		<td><?php echo $transaction->id ?></td>
+		<td><?php echo $transaction->id_transaction ?></td>
+		<td><?php echo $transaction->id_product ?></td>
+		<td><?php echo $transaction->status ?></td>
+		<td><?php echo $transaction->time ?></td>
+		<td><?php echo $transaction->id ?></td>
+		<td><?php echo $transaction->id ?></td>
+		
+	</tr>
+	<?php
+	}
+	
+	?>
+</table>
